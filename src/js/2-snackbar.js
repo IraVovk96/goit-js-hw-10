5826b7e455ed0c0d0a237012bf26143f12b318f7
IraVovk96 <148684180+IraVovk96@users.noreply.github.com>
2024-03-19 18:59:06 +0100

    add1

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,37 +1,37 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-function createPromise(delay, state) {
-    return new Promise((resolve, reject) => {
-      if (state === "fulfilled") {
-        setTimeout(() => {
-          resolve(delay);
-        }, delay);
-      } else if (state === "rejected") {
-        setTimeout(() => {
-          reject(delay);
-        }, delay);
+const formOfDalay = document.querySelector(".form");
+
+formOfDalay.addEventListener('submit', event => {
+  event.preventDefault();
+  const timer = event.currentTarget.elements.delay.value;
+  const radio = event.currentTarget.elements.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(timer, radio );
+      if (radio ==="fulfilled") {
+        resolve();
+      } else {
+        reject();
       }
-    });
-  }
-
-  document.querySelector(".form").addEventListener("submit", function(event) {
-    event.preventDefault(); 
-
-    const delay = parseInt(document.getElementById("delay").value); 
-    const state = document.querySelector('input[name="state"]:checked').value; 
-
-    createPromise(delay, state)
-      .then(delay => {
-        iziToast.success({
-          title: 'Success',
-          message: `✅ Fulfilled promise in ${delay}ms`
-        });
-      })
-      .catch(delay => {
-        iziToast.error({
-          title: 'Error',
-          message: `❌ Rejected promise in ${delay}ms`
-        });
-      });
+    }, timer);
   });
+
+  promise
+    .then(value => {
+      iziToast.success({  
+        color: 'green',
+        position: "topRight",
+        message: `✅ Fulfilled promise in ${timer}ms`
+      })
+    })
+    .catch(error => {
+      iziToast.error({ 
+        color: 'red',
+        position: "topRight",          
+        message: `❌ Rejected promise in ${timer}ms`
+      })
+    });
+});
